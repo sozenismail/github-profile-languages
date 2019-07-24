f655779a4b95fd0d5b0ff1605d900c94cc11813f
sozenismail <sozenismail97@gmail.com>
2019-07-24 13:02:34 +0300

    second

--- custom.js
+++ custom.js
@@ -8,30 +8,38 @@ var app=new Vue({
         return {
             langu: [],
             username: 'sozenismail',
-            lengh:0 
-            
+            lengh:0,
+             
             
         }
     },
     methods: {
         search() {
-            fetch(`https://api.github.com/users/${this.username}/repos`)
+            fetch(`https://api.github.com/users/${this.username}/repos?page=1&per_page=100`)
             .then((res) => {return res.json() })
             .then((res) => { 
                 
-              
+                
 
                  for (let sayi = 0; sayi < res.length; sayi++) {
                     
 
                    
                     this.langu.push(res[sayi].language);
-            
-            
-                }
 
+                    
+
+                    if(this.langu[sayi]==null)
+                    {
+                        this.langu[sayi]="NULL";
+                        
+                    }
+                   
+
+                 }
+
+                   this.lengh=res.length;
                   
-                 this.lengh=res.length;
                 }
             )
 
